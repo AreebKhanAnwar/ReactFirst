318275f583c4bd778e35daa624fd715aaaa478d6
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2025-04-18 11:28:12 +0500

    first proj

---
+++ proj/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import HeroSec from './component/HeroSec';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeroSec />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ proj/src/component/HeroSec.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Hero.css";
+const HeroSec = () => {
+  return (
+    <>
+        <div className='hero'>
+            <h1> This Is a Hero Section</h1>
+        </div>
+        <hr/>
+    </>
+  )
+}
+
+export default HeroSec
